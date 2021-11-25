e83797a1b68be202e11c3f693280f1703007cb8a
naveenben91 <naveen@fidisys.com>
2021-11-25 18:11:35 +0530

    google oauth implemented

---
+++ utils/auth.js
@@ -0,0 +1,16 @@
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const passport = require('passport');
+const jwtv = function (passport) {
+     let opts = {};
+    opts.jwtFromRequest = ExtractJwt.fromHeader('api-key');
+    opts.secretOrKey = 'S3CR3T_K3Y';//jwt secret key
+    passport.use(new JwtStrategy(opts, function (jwt_payload, done) {
+        // console.log(jwt_payload);
+        return done(null, jwt_payload);
+    }));
+};
+
+module.exports = {
+    jwtv,
+  }
---
+++ utils/oauth.js
@@ -0,0 +1,25 @@
+const passport = require('passport');
+const GoogleStrategy = require( 'passport-google-oauth2' ).Strategy;
+
+module.exports = function (passport){
+    passport.use(new GoogleStrategy({
+    clientID:     '720681694690-3a4pbnrtn3h8rv2mj90sa44qc6i1mhgm.apps.googleusercontent.com',
+    clientSecret: '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',
+    callbackURL: `http://${process.env.HOST}:${process.env.PORT}`+"/auth/google/callback",
+    passReqToCallback   : true
+  },
+  function(request, accessToken, refreshToken, profile, done) {
+     return done(null, profile);
+  }
+));
+
+passport.serializeUser(function(user, done) {
+    done(null, user);
+    }
+);
+
+passport.deserializeUser(function(user, done) {
+    done(null, user);
+    }
+);
+}
