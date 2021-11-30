5005f9940219160ba38e643d91464b377bf5a581
naveenben91 <naveen@fidisys.com>
2021-11-30 14:20:38 +0530

    in oauth.js changed

--- utils/oauth.js
+++ utils/oauth.js
@@ -5,7 +5,7 @@ module.exports = function (passport){
     passport.use(new GoogleStrategy({
     clientID:     '720681694690-3a4pbnrtn3h8rv2mj90sa44qc6i1mhgm.apps.googleusercontent.com',
     clientSecret: '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',
-    callbackURL: "http://fidigamesapi.herokuapp.com/auth/google/callback",
+    callbackURL: "https://fidigamesapi.herokuapp.com/auth/google/callback",
     passReqToCallback   : true
   },
   function(request, accessToken, refreshToken, profile, done) {
