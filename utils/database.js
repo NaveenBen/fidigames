ebf8fa460fdd71d9482d714e7face234567dc13b
naveenben91 <naveen@fidisys.com>
2021-12-02 16:02:22 +0530

    postgress connected

--- utils/database.js
+++ utils/database.js
@@ -8,6 +8,9 @@ const Sequelize = require('sequelize');
 const sequelize = new Sequelize(process.env.DATABASE_URL,{
     dialect: 'postgres',
     protocol: 'postgres',
+    dialectOptions: {
+        ssl: true
+    }
 });
 
 module.exports = sequelize;
