5788f151e3854f215d802be4548f9d3b97867050
naveenben91 <naveen@fidisys.com>
2021-12-02 15:52:50 +0530

    postgress connected

--- utils/database.js
+++ utils/database.js
@@ -1,8 +1,13 @@
 require('dotenv').config();
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize("fidigamesdb", "fidigames", "naveen786", {
-    host: './dev.sqlite',
-    dialect: 'sqlite',
+// const sequelize = new Sequelize("fidigamesdb", "fidigames", "naveen786", {
+//     host: './dev.sqlite',
+//     dialect: 'sqlite',
+// });
+
+const sequelize = new Sequelize(DATABASE_URL,{
+    dialect: 'postgres',
+    protocol: 'postgres',
 });
 
 module.exports = sequelize;
